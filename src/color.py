0e03d03984ebd2a48ff780adfc6189e2c5cc7de3
BasedElif <pedroalsim@gmail.com>
2022-05-15 11:05:30 +0100

    first commit

---
+++ src/color.py
@@ -0,0 +1,4 @@
+bgs = "#252422"
+fonts = "#FFFCF2"
+tbox = "#403D39"
+bgs2 = "#CCC5B9"
---
+++ src/main.py
@@ -0,0 +1,119 @@
+from tkinter import *
+from tkinter import ttk
+from color import *
+
+class app():
+
+    def __init__(self,wn):
+
+        self.window = wn
+
+        #window config
+        self.window.geometry("400x230")
+        self.window.config(bg = tbox)
+        self.window.title("mp3 tagger")
+        self.window.resizable(False, False)
+        #frames
+        self.f1 = Frame(self.window,bg = bgs, width = 380, height = 210)
+        self.f2 = Frame(self.window,bg = bgs, width = 380, height = 210)
+
+        self.f2.place(x = 10, y = 10)
+        self.f1.place(x = 10, y = 10)
+
+        #deploy frames
+        self.single_file_edit(self.f2)
+        self.main_menu(self.f1)
+
+
+
+
+
+
+
+
+
+        self.f1.tkraise()
+
+
+    def swap(self,frame):
+        frame.tkraise()
+
+
+    def single_file_edit(self,frame): #utils for single file edit
+
+
+        #labels
+
+        self.lfile_name = Label(frame, bg = bgs, fg = fonts,
+                          text = "File name:")
+        self.ltitle = Label(frame, bg = bgs, fg = fonts, text = "Title:")
+        self.lartist_name = Label(frame, bg = bgs, fg = fonts, text = "Artist:")
+        self.lalbum = Label(frame, bg = bgs, fg = fonts, text = "Album:")
+        self.lnumber = Label(frame, bg = bgs, fg = fonts, text = "Number:")
+
+        #entrys
+
+        self.file_name = Entry(frame, bg = tbox, fg = fonts, bd = 1,
+                        width = 26)
+        self.title = Entry(frame, bg = tbox, fg = fonts, bd = 1,
+                        width = 26)
+        self.artist_name = Entry(frame, bg = tbox, fg = fonts, bd = 1,
+                        width = 26)
+        self.album = Entry(frame, bg = tbox, fg = fonts, bd = 1, width = 26)
+
+        self.number = Spinbox(frame, bg = tbox, width = 4, increment = 1,
+                        from_ = 1, to = 999, bd = 1, fg = fonts)
+
+        #buttons
+
+        self.b1 = Button(self.f2, width = 7, height = 2, text = "<--",
+                    command = lambda: self.swap(self.f1), fg = bgs2, bg = bgs,
+                    bd = 0)
+
+        self.edit = Button(self.f2, width = 12, height = 2, text = "Edit",
+                    fg = fonts, bg = bgs)
+
+
+
+
+        #widget locations
+
+        self.file_name.place(x = 200, y = 20)
+        self.title.place(x = 200, y = 70)
+        self.artist_name.place(x = 200, y = 120)
+        self.album.place(x = 200, y = 170)
+        self.number.place(x = 80 , y = 120)
+
+        self.lfile_name.place(x = 135, y = 20)
+        self.ltitle.place(x = 135, y = 70)
+        self.lartist_name.place(x = 135, y = 120)
+        self.lalbum.place(x = 135, y = 170)
+        self.lnumber.place(x = 15, y = 120)
+
+        self.b1.place(x = 0, y = 0)
+        self.edit.place(x = 20, y = 160)
+
+
+    def main_menu(self,frame): #main menu widgets
+
+        self.b2 = Button(self.f1, width = 7,height = 2, text = "swap to 2",
+                  command = lambda: self.swap(self.f2))
+
+
+        self.b2.place(x = 40,y = 60)
+
+
+
+
+
+
+
+root = Tk()
+editor = app(root)
+
+
+
+
+
+
+root.mainloop()
